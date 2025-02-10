7dba5c4690d57a9ef7ab61578b771cf2645ad49e
khaled elbhdaly <155319819+kelbhdaly@users.noreply.github.com>
2025-02-10 14:46:50 +0200

    Generics Swap

---
+++ Session01/Helper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session01
+{
+    internal static class Helper<T>
+    {
+        public static void Swap( ref T x , ref T y)
+        {
+            T temp = x;
+            x = y;
+            y = temp;
+        }
+        
+        //public static void Swap( ref decimal x , ref decimal y)
+        //{
+        //    decimal temp = x;
+        //    x = y;
+        //    y = temp;
+        //}
+        
+        //public static void Swap( ref Point x , ref Point y)
+        //{
+        //    Point temp = x;
+        //    x = y;
+        //    y = temp;
+        //}
+
+    }
+}
--- Session01/Program.cs
+++ Session01/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Generics
+
+            int a = 10, b = 20;
+            Console.WriteLine($"A = {a}");
+            Console.WriteLine($"B = {b}");
+
+            Helper<int>.Swap(ref a, ref b);
+
+            Console.WriteLine($"B = {b}");
+            Console.WriteLine($"A = {a}");
+
+
+            decimal L = 10.20M, M = 20.10M;
+
+            Console.WriteLine($"L = {L}"); //10.20
+            Console.WriteLine($"M = {M}"); //20.10
+
+            Helper<Decimal>.Swap(ref M, ref L);
+
+            Console.WriteLine($"L = {L}"); //20.10
+            Console.WriteLine($"M = {M}"); //10.20
+
+
+            Point point01 = new Point(10, 20);
+            Point point02 = new Point(100, 200);
+
+            Console.WriteLine($"Point 01 = {point01}");//(10 , 20)
+            Console.WriteLine($"Point 02 = {point02}");//(100 , 200)
+
+            Helper<Point>.Swap(ref point02, ref point01);
+
+            Console.WriteLine($"Point 01 = {point01}"); //(100 , 200)
+            Console.WriteLine($"Point 02 = {point02}"); //(10 , 20) 
+            #endregion
+
         }
     }
 }
